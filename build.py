1019b02d4a979faf72a4507c41c17c45061614c4
Nag Varun Chunduru <cnv1989@gmail.com>
2014-03-15 16:01:29 -0400

    Added minified version for block column maker.

--- build.py
+++ build.py
@@ -26,6 +26,7 @@ def update_version():
 
 
 def update_transect_min():
+	"""Transect min"""
 	transect_min = "./transect_maker/html/transect_min.html"
 	transect_old_built = "transect-maker-built." + old_version() + ".js"
 	transect_new_built = "transect-maker-built." + new_version() + ".js"
@@ -38,7 +39,6 @@ def update_transect_built():
 	transect_built = "./transect-maker-built.js"
 	transect_new_built = "transect_maker/js/transect-maker-built." + new_version() + ".js"
 	cmd = "r.js -o " + transect_built + " name=" + main + " out=" + transect_new_built
-	print cmd
 	os.system(cmd)
 
 
@@ -53,8 +53,36 @@ def update_transect():
 	update_transect_built()
 
 
+def update_block_min():
+	block_min = "./block_column_maker/html/block_column_maker_min.html"
+	block_old_built = "block-column-maker-built." + old_version() + ".js"
+	block_new_built = "block-column-maker-built." + new_version() + ".js"
+	cmd = "sed s/" + block_old_built + "/" + block_new_built + "/g " + block_min + " > " + block_min + ".bak && mv " + block_min + ".bak " + " " + block_min
+	os.system(cmd)
+
+
+def update_block_built():
+	main = "block_column_maker/js/block_app"
+	block_built = "./block-column-maker-built.js"
+	block_new_built = "block_column_maker/js/block-column-maker-built." + new_version() + ".js"
+	cmd = "r.js -o " + block_built + " name=" + main + " out=" + block_new_built
+	os.system(cmd)
+
+
+def clean_block():
+	cmd = "rm block_column_maker/js/block-maker-built.*.js"
+	os.system(cmd)
+
+
+def update_block():
+	clean_block()
+	update_block_min()
+	update_block_built()
+
+
 def main():
 	update_transect()
+	update_block()
 	update_version()
 
 main()
