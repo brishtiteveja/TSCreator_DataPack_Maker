c729994900b62f9f80bda5618afbc3d9149e3aa4
Nag Varun Chunduru <cnv1989@gmail.com>
2014-06-22 10:04:31 -0700

    Updated min.

--- lithology_column_maker/js/lithology_app.js
+++ lithology_column_maker/js/lithology_app.js
@@ -1,134 +1,134 @@
 requirejs.config({
-	paths: {
+    paths: {
 
-		// Base components
-		baseView: "/commons/js/views/base_view",
-		baseModel: "/commons/js/models/base_model",
-		baseCollection: "/commons/js/collections/base_collection",
+        // Base components
+        baseView: "/commons/js/views/base_view",
+        baseModel: "/commons/js/models/base_model",
+        baseCollection: "/commons/js/collections/base_collection",
 
-		ruler: "/commons/js/models/ruler",
-		rulerView: "/commons/js/views/ruler_view",
+        ruler: "/commons/js/models/ruler",
+        rulerView: "/commons/js/views/ruler_view",
 
-		// Default
-		defaultOb: "../../commons/js/models/default_ob",
-		defaultView: "/commons/js/views/default_view",
+        // Default
+        defaultOb: "/commons/js/models/default_ob",
+        defaultView: "/commons/js/views/default_view",
 
-		// App
-		app: "/commons/js/models/app",
-
-
-		// Settings
-		settings: "/commons/js/models/settings",
-
-		// Cursor
-		cursorView: "/commons/js/views/cursor_view",
-		cursor: "/commons/js/models/cursor",
-
-		// Markers
-		markersView: "/commons/js/views/markers_view",
-		markerView: "/commons/js/views/marker_view",
-		marker: "/commons/js/models/marker",
-		markers: "/commons/js/collections/markers",
-
-		// Zones
-		zoneView: "/commons/js/views/zone_view",
-		zonesView: "/commons/js/views/zones_view",
-		zone: "/commons/js/models/zone",
-		zones: "/commons/js/collections/zones",
+        // App
+        app: "/commons/js/models/app",
 
 
-		// Animation
-		animation: "../../commons/js/models/animation",
-		animationView: "../../commons/js/views/animation_view",
+        // Settings
+        settings: "/commons/js/models/settings",
+
+        // Cursor
+        cursorView: "/commons/js/views/cursor_view",
+        cursor: "/commons/js/models/cursor",
+
+        // Markers
+        markersView: "/commons/js/views/markers_view",
+        markerView: "/commons/js/views/marker_view",
+        marker: "/commons/js/models/marker",
+        markers: "/commons/js/collections/markers",
+
+        // Zones
+        zoneView: "/commons/js/views/zone_view",
+        zonesView: "/commons/js/views/zones_view",
+        zone: "/commons/js/models/zone",
+        zones: "/commons/js/collections/zones",
 
 
-		// Image
-		imageView: "/commons/js/views/image_view",
-		imageOb: "/commons/js/models/image",
+        // Animation
+        animation: "/commons/js/models/animation",
+        animationView: "/commons/js/views/animation_view",
 
-		// Data Loader
-		loader: "/lithology_column_maker/js/utils/loader",
 
-		// Data Exporter
-		exporter: "/lithology_column_maker/js/utils/exporter",
-		dataExportView: "/lithology_column_maker/js/views/data_export_view",
+        // Image
+        imageView: "/commons/js/views/image_view",
+        imageOb: "/commons/js/models/image",
 
-		// Lithology column models 
-		lithology: "/lithology_column_maker/js/models/lithology",
-		lithologys: "/lithology_column_maker/js/collections/lithologys",
+        // Data Loader
+        loader: "/lithology_column_maker/js/utils/loader",
 
-		lithologyMarker: "/lithology_column_maker/js/models/lithology_marker",
-		lithologyMarkers: "/lithology_column_maker/js/collections/lithology_markers",
+        // Data Exporter
+        exporter: "/lithology_column_maker/js/utils/exporter",
+        dataExportView: "/lithology_column_maker/js/views/data_export_view",
 
-		lithologyGroupMarker: "/lithology_column_maker/js/models/lithology_group_marker",
-		lithologyGroupMarkers: "/lithology_column_maker/js/collections/lithology_group_markers",
+        // Lithology column models 
+        lithology: "/lithology_column_maker/js/models/lithology",
+        lithologys: "/lithology_column_maker/js/collections/lithologys",
 
-		lithologyGroup: "/lithology_column_maker/js/models/lithology_group",
-		lithologyGroups: "/lithology_column_maker/js/collections/lithology_groups",
+        lithologyMarker: "/lithology_column_maker/js/models/lithology_marker",
+        lithologyMarkers: "/lithology_column_maker/js/collections/lithology_markers",
 
-		lithologyColumn: "/lithology_column_maker/js/models/lithology_column",
-		lithologyColumns: "/lithology_column_maker/js/collections/lithology_columns",
+        lithologyGroupMarker: "/lithology_column_maker/js/models/lithology_group_marker",
+        lithologyGroupMarkers: "/lithology_column_maker/js/collections/lithology_group_markers",
 
-		lithologyView: "/lithology_column_maker/js/views/lithology_view",
-		lithologyMarkerView: "/lithology_column_maker/js/views/lithology_marker_view",
+        lithologyGroup: "/lithology_column_maker/js/models/lithology_group",
+        lithologyGroups: "/lithology_column_maker/js/collections/lithology_groups",
 
-		lithologyGroupView: "/lithology_column_maker/js/views/lithology_group_view",
-		lithologyGroupMarkerView: "/lithology_column_maker/js/views/lithology_group_marker_view",
+        lithologyColumn: "/lithology_column_maker/js/models/lithology_column",
+        lithologyColumns: "/lithology_column_maker/js/collections/lithology_columns",
 
-		lithologyColumnView: "/lithology_column_maker/js/views/lithology_column_view",
-		lithologyColumnsView: "/lithology_column_maker/js/views/lithology_columns_view",
+        lithologyView: "/lithology_column_maker/js/views/lithology_view",
+        lithologyMarkerView: "/lithology_column_maker/js/views/lithology_marker_view",
 
-		// Libraries
-		polyK: "../../commons/js/lib/polyK",
-		raphael: "../../commons/js/lib/raphael",
+        lithologyGroupView: "/lithology_column_maker/js/views/lithology_group_view",
+        lithologyGroupMarkerView: "/lithology_column_maker/js/views/lithology_group_marker_view",
 
-		// File System
-		file: "/file_system/js/models/file",
-		files: "/file_system/js/collections/files",
-		fileView: "/file_system/js/views/file_view",
-		filesView: "/file_system/js/views/files_view",
-		fileSystem: "/file_system/js/models/file_system",
-		fileSystemView: "/file_system/js/views/file_system_view",
+        lithologyColumnView: "/lithology_column_maker/js/views/lithology_column_view",
+        lithologyColumnsView: "/lithology_column_maker/js/views/lithology_columns_view",
 
-		// 
-		lithologyAppView: "/lithology_column_maker/js/views/lithology_app_view",
+        // Libraries
+        polyK: "/commons/js/lib/polyK",
+        raphael: "/commons/js/lib/raphael",
 
-		// Points
-		pointView: "../../lithology_2D/js/views/point_view",
-		point: "../../lithology_2D/js/models/point",
-		points: "../../lithology_2D/js/collections/points",
+        // File System
+        file: "/file_system/js/models/file",
+        files: "/file_system/js/collections/files",
+        fileView: "/file_system/js/views/file_view",
+        filesView: "/file_system/js/views/files_view",
+        fileSystem: "/file_system/js/models/file_system",
+        fileSystemView: "/file_system/js/views/file_system_view",
 
-		// Polygon
-		polygonView: "../../lithology_2D/js/views/polygon_view",
-		polygonsView: "../../lithology_2D/js/views/polygons_view",
-		polygon: "../../lithology_2D/js/models/polygon",
-		polygons: "../../lithology_2D/js/collections/polygons",
+        // 
+        lithologyAppView: "/lithology_column_maker/js/views/lithology_app_view",
 
-		// Map View
-		mapView: "../../lithology_2D/js/views/map_view",
+        // Points
+        pointView: "/lithology_2D/js/views/point_view",
+        point: "/lithology_2D/js/models/point",
+        points: "/lithology_2D/js/collections/points",
 
-		lithology2dView: "../../lithology_2D/js/views/lithology_2d_view",
+        // Polygon
+        polygonView: "/lithology_2D/js/views/polygon_view",
+        polygonsView: "/lithology_2D/js/views/polygons_view",
+        polygon: "/lithology_2D/js/models/polygon",
+        polygons: "/lithology_2D/js/collections/polygons",
 
-		// Reference Column Data Loader
-		referenceColumnLoader: "/reference_column_maker/js/utils/loader",
+        // Map View
+        mapView: "/lithology_2D/js/views/map_view",
 
-		// Reference column
-		referenceColumn: "/reference_column_maker/js/models/reference_column",
-		referenceBlock: "/reference_column_maker/js/models/reference_block",
-		referenceBlocks: "/reference_column_maker/js/collections/reference_blocks",
-		referenceBlockMarker: "/reference_column_maker/js/models/reference_block_marker",
-		referenceBlockMarkers: "/reference_column_maker/js/collections/reference_block_markers",
-		referenceBlockColumn: "/reference_column_maker/js/models/reference_block_column",
-		referenceBlockColumns: "/reference_column_maker/js/collections/reference_block_columns",
-		referenceBlockView: "/reference_column_maker/js/views/reference_block_view",
-		referenceBlockMarkerView: "/reference_column_maker/js/views/reference_block_marker_view",
-		referenceBlockColumnView: "/reference_column_maker/js/views/reference_block_column_view",
-		referenceBlockColumnsView: "/reference_column_maker/js/views/reference_block_columns_view",
-		referenceColumnSideView: "/reference_column_maker/js/views/reference_column_side_view",
-	},
+        lithology2dView: "/lithology_2D/js/views/lithology_2d_view",
+
+        // Reference Column Data Loader
+        referenceColumnLoader: "/reference_column_maker/js/utils/loader",
+
+        // Reference column
+        referenceColumn: "/reference_column_maker/js/models/reference_column",
+        referenceBlock: "/reference_column_maker/js/models/reference_block",
+        referenceBlocks: "/reference_column_maker/js/collections/reference_blocks",
+        referenceBlockMarker: "/reference_column_maker/js/models/reference_block_marker",
+        referenceBlockMarkers: "/reference_column_maker/js/collections/reference_block_markers",
+        referenceBlockColumn: "/reference_column_maker/js/models/reference_block_column",
+        referenceBlockColumns: "/reference_column_maker/js/collections/reference_block_columns",
+        referenceBlockView: "/reference_column_maker/js/views/reference_block_view",
+        referenceBlockMarkerView: "/reference_column_maker/js/views/reference_block_marker_view",
+        referenceBlockColumnView: "/reference_column_maker/js/views/reference_block_column_view",
+        referenceBlockColumnsView: "/reference_column_maker/js/views/reference_block_columns_view",
+        referenceColumnSideView: "/reference_column_maker/js/views/reference_column_side_view",
+    },
 });
 
 
-requirejs(["lithologyAppView"], function(LithologyAppView) {
-	var lithologyAppView = new LithologyAppView();
+requirejs(["lithologyAppView"], function (LithologyAppView) {
+    var lithologyAppView = new LithologyAppView();
 });
