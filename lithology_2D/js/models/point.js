2f3ed78c21c326bace743ae39e35f420dcc8e1f5
Nag Varun Chunduru <nchunduru@hearsaycorp.com>
2014-06-29 19:53:20 -0700

    Made default lat lon value 0

--- lithology_2D/js/models/point.js
+++ lithology_2D/js/models/point.js
@@ -1,25 +1,25 @@
-define(["baseModel"], function(BaseModel) {
-	var Point = BaseModel.extend({
-		classname: "Point",
-		constructor: function(params, app) {
-			var attrs = [{
-				edit: false,
-				name: params.name || _.uniqueId("X"),
-				x: params.x ? parseInt(params.x) : 0,
-				y: params.y ? parseInt(params.y) : 0,
-				lat: params.lat,
-				lon: params.lon,
-				app: app
-			}];
-			BaseModel.apply(this, attrs);
-		}
-	});
+define(["baseModel"], function (BaseModel) {
+    var Point = BaseModel.extend({
+        classname: "Point",
+        constructor: function (params, app) {
+            var attrs = [{
+                edit: false,
+                name: params.name || _.uniqueId("X"),
+                x: params.x ? parseInt(params.x) : 0,
+                y: params.y ? parseInt(params.y) : 0,
+                lat: params.lat || 0,
+                lon: params.lon || 0,
+                app: app
+            }];
+            BaseModel.apply(this, attrs);
+        }
+    });
 
-	Point.prototype.toJSON = function() {
-		var json = _.clone(this.attributes);
-		delete json["app"];
-		return json;
-	}
+    Point.prototype.toJSON = function () {
+        var json = _.clone(this.attributes);
+        delete json["app"];
+        return json;
+    }
 
-	return Point;
+    return Point;
 });
